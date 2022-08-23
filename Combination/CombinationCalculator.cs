77321e16f9d1617f1dcba812bc3717ea514cdf4f
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 13:15:43 +0430

    Initial Repo

---
+++ Combination/CombinationCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combination
+{
+    public class CombinationCalculator
+    {
+        public int Calculate(int n, int k)
+        {
+            if (n == k || k == 0)
+                return 1;
+            if (n <= 0 && k >= 1)
+                return 0;
+
+            return Calculate(n - 1, k - 1) + Calculate(n - 1, k);
+        }
+    }
+}
---
+++ Combination/Program.cs
@@ -0,0 +1,12 @@
+﻿using Combination;
+
+Console.WriteLine("Please enter n :");
+var Inputn = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter k :");
+var Inputk = int.Parse(Console.ReadLine());
+
+var combination = new CombinationCalculator();
+var result = combination.Calculate(Inputn, Inputk);
+
+Console.WriteLine($" the rsult is  : {result}");
